67ff03acece07329d4e03d1da24088627a0c6668
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-11-13 15:51:53 -0500

    Removed env files and update interface export

--- wolox-books/src/app/interfaces/user.interface.ts
+++ wolox-books/src/app/interfaces/user.interface.ts
@@ -1,11 +1,7 @@
-interface IUser {
+export interface IUser {
   'first_name': string;
   'last_name': string;
   'email': string;
   'password': string;
   'password_confirmation': string;
-}
-
-export {
-  IUser,
 }
