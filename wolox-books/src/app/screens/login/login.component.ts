64ad548c38cbafd6b3f4fc8783de2641c4b2be06
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-11-17 17:22:46 -0500

    Session management and logout implemented

--- wolox-books/src/app/screens/login/login.component.ts
+++ wolox-books/src/app/screens/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { Router } from '@angular/router';
 import patternPasswordValidation from 'src/app/constants/pattern-password-validation.constant';
 import { IUserBasic, IUserHTTPResponse } from 'src/app/interfaces/user.interface';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -19,6 +20,7 @@ export class LoginComponent {
     private fb: FormBuilder,
     private userService: UserService,
     private router: Router,
+    private localStorageService: LocalStorageService,
   ) {
     this.loginForm = fb.group({
       'email': [ null, [ Validators.required, Validators.email ] ],
@@ -28,10 +30,10 @@ export class LoginComponent {
 
   loginUser(user: IUserBasic) {
     this.userService.loginUser(user).subscribe((respose: HttpResponse<IUserHTTPResponse>) => {
-      const keys = respose.headers.keys();
-      const headers = keys.map(key => `${key}: ${respose.headers.get(key)}`);
-      console.log('access-token', respose.headers.get('access-token'))
-      this.router.navigate(['/'])
+      this.localStorageService.storeOnLocalStorage('access-token', respose.headers.get('access-token') as string)
+      this.localStorageService.storeOnLocalStorage('client', respose.headers.get('client') as string)
+      this.localStorageService.storeOnLocalStorage('uid', respose.headers.get('uid') as string)
+      this.router.navigate(['app', 'list'])
     });
   }
 
---
+++ wolox-books/src/app/services/local-storage.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  private _keysStored: Set<string> = new Set([]);
+
+  get keysStored(): Set<string> {
+    return this._keysStored;
+  }
+
+  storeOnLocalStorage(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  clearStorage(): void {
+    Object.keys(localStorage).forEach((key: string) => localStorage.removeItem(key))
+  }
+}
