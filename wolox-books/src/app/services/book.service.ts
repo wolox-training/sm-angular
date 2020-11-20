aaa6fad215382fd8c71dee01d9f0b33737df05c4
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-11-20 14:00:53 -0500

    Shopping cart added

--- wolox-books/src/app/services/book.service.ts
+++ wolox-books/src/app/services/book.service.ts
@@ -1,17 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IAllBooksHTTPResponse, IBookDetailResponse } from '../interfaces/book.interface';
+import { IAllBooksHTTPResponse, IBookDetailResponse, IBooksResponse } from '../interfaces/book.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  private BookCart = new BehaviorSubject<IBooksResponse[]>([]);
+
   constructor(
     private _http: HttpClient,
   ) { }
+
+  addBookToCart(book: IBooksResponse) {
+    const newCurrentBooks: IBooksResponse[] = [...this.BookCart.value, book];
+    this.BookCart.next(newCurrentBooks)
+  }
+
+  get bookCart(): Observable<IBooksResponse[]> {
+    return this.BookCart.asObservable();
+  }
 
   getAllBooks(): Observable<IAllBooksHTTPResponse> {
     return this._http.get<IAllBooksHTTPResponse>(`${environment.api}/books`);
